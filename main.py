bab0777d77155ea0804fb0375b4c678d8176f28f
marios.koundouris <marioskoundouris@gmail.com>
2022-11-02 15:10:16 +0200

    test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import requests
 import json
@@ -39,34 +40,6 @@ else:
         st.write(f'Feels like: {data["current"]["feelslike_c"]} ℃')
 
     with col2:
-
-        st.write(f'Temp in Celcius: {data["current"]["temp_c"]}')
-        st.write(f'Temp in Farenheit: {data["current"]["temp_f"]}')
-        st.write(f'Condition: {data["current"]["condition"]["text"]}')
-        st.image(f'http:{data["current"]["condition"]["icon"]}')
-        st.metric(label = "Humidity", value = f'{data["current"]["humidity"]}')
-
-    st.info('⛅ Current weather or realtime weather API method allows a user to get up to date current weather information in json and xml. The data is returned as a Current Object.')
-
-    components.html(
-        """
-        <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0" target="_blank"></a>
-        """
-    )
-else:
-    data = json.loads(result)
-    col1, col2 = st.columns(2)
-
-    with col1:
-
-        st.write(f'Name: {data["location"]["name"]}')
-        st.write(f'Region: {data["location"]["region"]}')
-        st.write(f'Country: {data["location"]["country"]}')
-        st.write(f'Local Time: {data["location"]["localtime"]}')
-        st.metric(label="wind_kph", value= f'{data["current"]["wind_kph"]}')
-        st.write(f'Feels like: {data["current"]["feelslike_c"]} ℃')
-
-    with col2: 
 
         st.write(f'Temp in Celcius: {data["current"]["temp_c"]}')
         st.write(f'Temp in Farenheit: {data["current"]["temp_f"]}')
